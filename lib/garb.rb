1dc9c73f6e70209fb860283533010bf2af5a3756
Jory Z. Ruscio <jzruscio@gmail.com>
2013-06-18 22:00:08 -0700

    fix require statement for bundler

--- lib/garb.rb
+++ lib/garb.rb
@@ -40,7 +40,7 @@ require 'garb/management/web_property'
 require 'garb/management/profile'
 require 'garb/management/goal'
 
-require 'support'
+require File.expand_path('../support', __FILE__)
 
 module Garb
   GA = "http://schemas.google.com/analytics/2008"
